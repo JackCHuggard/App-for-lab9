6e8781972d56846c01f8a0f90c20a5c3459c124d
jackchuggard <jahu9463@colorado.edu>
2018-11-08 08:25:33 -0700

    changing the dbconfig for online deployment

--- Lab9/database.js
+++ Lab9/database.js
@@ -7,15 +7,8 @@
 // dbConfig is an Object (key-value pairs) that houses information of the PostgreSQL database you want to connect to.
 var pgp = require('pg-promise')();
 
-const dbConfig = {
-   host: 'localhost',
-   port: 5432,
-   database: 'lab6',
-   user: 'jackhuggard',
-   password: 'Penguin7060' // TODO: Fill in your PostgreSQL password here.
-                // Use empty string if you did not set a password
-};
-
 var​ dbConfig = process.env.DATABASE_URL;
+
+var db = pgp(dbConfig);
 
 module.exports = db;
